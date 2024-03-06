fe3598d4fee5e7f6f446db695aa6e13483ca83cd
smail <smailcmail@gmail.com>
2024-03-06 04:11:37 +0300

    flags -> .env

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,47 +2,54 @@ package main
 
 import (
 	"errors"
-	"flag"
 	"fmt"
 	_ "github.com/go-park-mail-ru/2024_1_Cyberkotletki/docs"
 	"github.com/go-park-mail-ru/2024_1_Cyberkotletki/internal/app"
 	"github.com/go-park-mail-ru/2024_1_Cyberkotletki/internal/config"
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"path/filepath"
-)
-
-var (
-	listenAddress string
-	listenPort    string
-	serverMode    config.ServerMode
-	genSwagger    bool
-	staticFolder  string
-	cors          string
+	"strconv"
 )
 
 // @title API Киноскопа
 // @version 1.0
 func main() {
+	logger := log.New(os.Stdout, "server: ", log.LstdFlags)
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Не удалось загрузить .env файл. Убедитесь, что он расположен в корне проекта")
+	}
 	staticDefaultFolder, _ := os.Getwd()
-	flag.StringVar(&listenAddress, "listen-address", "localhost", "Адрес сервера")
-	flag.StringVar(&listenPort, "listen-port", "8000", "Порт сервера")
-	flag.IntVar((*int)(&serverMode), "server-mode", 2, "0 = deploy\n1 = test\n2 = dev")
-	flag.BoolVar(&genSwagger, "generate-swagger", true, "true = сгенерировать swagger")
-	flag.StringVar(&staticFolder, "static-folder", filepath.Join(staticDefaultFolder, "assets", "examples", "static"), "путь до папки со статикой")
-	flag.StringVar(&staticFolder, cors, "http://localhost", "Параметр Access-Control-Allow-Origin")
-	flag.Parse()
+	listenAddress := os.Getenv("LISTEN_ADDRESS")
+	listenPort := os.Getenv("LISTEN_PORT")
+	serverMode, err := strconv.ParseInt(os.Getenv("SERVER_MODE"), 10, 32)
+	if err != nil || serverMode > 2 || serverMode < 0 {
+		log.Fatal("Неправильное использование параметра SERVER_MODE")
+	}
+	var genSwagger bool
+	if swagger := os.Getenv("GEN_SWAGGER"); swagger == "true" {
+		genSwagger = true
+	} else {
+		genSwagger = false
+	}
+	staticFolder := os.Getenv("STATIC_DIR")
+	if staticFolder == "" {
+		staticFolder = filepath.Join(staticDefaultFolder, "assets", "examples", "static")
+	}
+	cors := os.Getenv("CORS")
 	params := config.InitParams{
 		Addr:         fmt.Sprintf("%s:%s", listenAddress, listenPort),
-		Mode:         serverMode,
+		Mode:         config.ServerMode(serverMode),
 		GenSwagger:   genSwagger,
 		StaticFolder: staticFolder,
 		CORS:         cors,
 	}
 
-	logger := log.New(os.Stdout, "server: ", log.LstdFlags)
+	logger.Printf("Параметры запуска сервера: %v \n", params)
 
 	done := make(chan bool, 1)
 	quit := make(chan os.Signal, 1)
@@ -54,7 +61,7 @@ func main() {
 
 	logger.Println("Сервер запущен по адресу", params.Addr)
 	if err := app.Run(server, params); err != nil && !errors.Is(err, http.ErrServerClosed) {
-		logger.Fatalf("Невозможно обрабатывать запросы по адресу %s: %v\n", params.Addr, err)
+		logger.Fatalf("Сервер перестал обрабатывать запросы по адресу %s: %v\n", params.Addr, err)
 	}
 
 	<-done
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22.0
 require (
 	github.com/google/uuid v1.6.0
 	github.com/gorilla/mux v1.8.1
+	github.com/joho/godotenv v1.5.1
 	github.com/swaggo/http-swagger/v2 v2.0.2
 	github.com/swaggo/swag v1.16.3
 	golang.org/x/crypto v0.21.0
