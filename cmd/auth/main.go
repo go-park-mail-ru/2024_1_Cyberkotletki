0d1fdd27c15e7d581352ebadfbc859638e3edb57
Aleksandr Batovkin <smailcmail@gmail.com>
2024-05-31 02:11:48 +0300

    index optimizations + improvements

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"github.com/go-park-mail-ru/2024_1_Cyberkotletki/config"
 	"github.com/go-park-mail-ru/2024_1_Cyberkotletki/internal/delivery/grpc/auth"
@@ -10,75 +9,16 @@ import (
 	"github.com/go-park-mail-ru/2024_1_Cyberkotletki/internal/repository/redis"
 	"github.com/go-park-mail-ru/2024_1_Cyberkotletki/internal/usecase/service"
 	"github.com/go-park-mail-ru/2024_1_Cyberkotletki/pkg/connector"
-	"github.com/joho/godotenv"
 	"github.com/labstack/gommon/log"
-	"github.com/mcuadros/go-defaults"
 	"google.golang.org/grpc"
-	"gopkg.in/yaml.v3"
 	"net"
-	"os"
 	"os/signal"
 	"syscall"
 )
 
-func GenerateExampleConfig() {
-	// Создание экземпляра структуры с использованием значений по умолчанию из тегов default
-	var conf config.AuthConfig
-	defaults.SetDefaults(&conf)
-
-	yamlData, err := yaml.Marshal(&conf)
-	if err != nil {
-		fmt.Printf("Ошибка при маршализации YAML: %v\n", err)
-		return
-	}
-	file, err := os.Create("config_auth.yaml")
-	if err != nil {
-		fmt.Printf("Ошибка при создании файла: %v\n", err)
-		return
-	}
-	_, err = file.Write(yamlData)
-	if err != nil {
-		fmt.Printf("Ошибка при записи в файл: %v\n", err)
-		return
-	}
-	err = file.Close()
-	if err != nil {
-		fmt.Printf("Ошибка при записи в файл: %v\n", err)
-		return
-	}
-
-	fmt.Println("Конфигурационный файл успешно создан.")
-}
-
-func ParseParams() config.AuthConfig {
-	var cfg config.AuthConfig
-	// читаем конфиг
-	yamlFile, err := os.ReadFile("config_auth.yaml")
-	if err != nil {
-		log.Fatalf("Ошибка при чтении конфига сервера: %v", err)
-	}
-	err = yaml.Unmarshal(yamlFile, &cfg)
-	if err != nil {
-		log.Fatalf("Ошибка при парсинге конфига сервера: %v", err)
-	}
-	// читаем переменные окружения
-	err = godotenv.Load()
-	if err != nil {
-		fmt.Println("Ошибка загрузки .env файла")
-	}
-	cfg.Redis.Password = os.Getenv("REDIS_PASSWORD")
-	return cfg
-}
-
 func main() {
-	genCfg := flag.Bool("generate-example-config", false, "Генерирует пример конфига, с которым умеет работать сервер")
-	flag.Parse()
-	if *genCfg {
-		GenerateExampleConfig()
-		return
-	}
 	logger := log.New("Auth Microservice: ")
-	params := ParseParams()
+	params := config.ParseAuthServiceParams()
 	logger.Printf("Параметры запуска сервера: %v \n", params)
 
 	redisConn, err := connector.GetRedisConnector(params.Redis.Addr, params.Redis.Password, params.Redis.DB)
--- config/config.go
+++ config/config.go
@@ -2,6 +2,11 @@ package config
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/labstack/gommon/log"
+	"github.com/mcuadros/go-defaults"
+	"gopkg.in/yaml.v3"
+	"os"
 )
 
 type Server struct {
@@ -78,4 +83,107 @@ func (cfg *PostgresDatabase) GetConnectURL() string {
 	return fmt.Sprintf(
 		"postgres://%s:%s@%s:%d/kinoskop?sslmode=disable",
 		cfg.User, cfg.Pass, cfg.IP, cfg.Port)
+}
+
+func GenerateExampleConfigs() {
+	type cfg struct {
+		config   any
+		filename string
+	}
+
+	configs := []cfg{
+		{Config{}, "config.yaml"},
+		{StaticConfig{}, "config_static.yaml"},
+		{AuthConfig{}, "config_auth.yaml"},
+	}
+	for _, conf := range configs {
+		defaults.SetDefaults(conf.config)
+		yamlData, err := yaml.Marshal(conf.config)
+		if err != nil {
+			fmt.Printf("Ошибка при маршализации YAML: %v\n", err)
+			return
+		}
+		file, err := os.Create("config.yaml")
+		if err != nil {
+			fmt.Printf("Ошибка при создании файла: %v\n", err)
+			return
+		}
+		_, err = file.Write(yamlData)
+		if err != nil {
+			fmt.Printf("Ошибка при записи в файл: %v\n", err)
+			return
+		}
+		err = file.Close()
+		if err != nil {
+			fmt.Printf("Ошибка при записи в файл: %v\n", err)
+			return
+		}
+
+		fmt.Println("Конфигурационный файл успешно создан.")
+	}
+}
+
+func ParseCoreServiceParams() Config {
+	var cfg Config
+	// читаем конфиг
+	yamlFile, err := os.ReadFile("config.yaml")
+	if err != nil {
+		log.Fatalf("Ошибка при чтении конфига сервера: %v", err)
+	}
+	err = yaml.Unmarshal(yamlFile, &cfg)
+	if err != nil {
+		log.Fatalf("Ошибка при парсинге конфига сервера: %v", err)
+	}
+	// читаем переменные окружения
+	err = godotenv.Load()
+	if err != nil {
+		fmt.Println(".env файл отсутствует, переменные будут загружены из переменных окружения")
+	}
+	cfg.Postgres.User = os.Getenv("POSTGRES_USER")
+	cfg.Postgres.Pass = os.Getenv("POSTGRES_PASSWORD")
+	cfg.ContentSecretKey = os.Getenv("CONTENT_SECRET_KEY")
+	return cfg
+}
+
+func ParseAuthServiceParams() AuthConfig {
+	var cfg AuthConfig
+	// читаем конфиг
+	yamlFile, err := os.ReadFile("config_auth.yaml")
+	if err != nil {
+		log.Fatalf("Ошибка при чтении конфига сервера: %v", err)
+	}
+	err = yaml.Unmarshal(yamlFile, &cfg)
+	if err != nil {
+		log.Fatalf("Ошибка при парсинге конфига сервера: %v", err)
+	}
+	// читаем переменные окружения
+	err = godotenv.Load()
+	if err != nil {
+		fmt.Println("Ошибка загрузки .env файла")
+	}
+	cfg.Redis.Password = os.Getenv("REDIS_PASSWORD")
+	return cfg
+}
+
+func ParseStaticServiceParams() StaticConfig {
+	var cfg StaticConfig
+	// читаем конфиг
+	yamlFile, err := os.ReadFile("config_static.yaml")
+	if err != nil {
+		log.Fatalf("Ошибка при чтении конфига сервера: %v", err)
+	}
+	err = yaml.Unmarshal(yamlFile, &cfg)
+	if err != nil {
+		log.Fatalf("Ошибка при парсинге конфига сервера: %v", err)
+	}
+	// читаем переменные окружения
+	err = godotenv.Load()
+	if err != nil {
+		fmt.Println("Ошибка загрузки .env файла")
+	}
+	cfg.Postgres.User = os.Getenv("POSTGRES_USER")
+	cfg.Postgres.Pass = os.Getenv("POSTGRES_PASSWORD")
+	cfg.S3.AccessKeyID = os.Getenv("S3_ACCESS_KEY_ID")
+	cfg.S3.SecretAccessKey = os.Getenv("S3_SECRET_ACCESS_KEY")
+	return cfg
 }
